d6fd2e329ebaa863bf2c38cd52d79bff3adcd8cf
YJ <yoojungpark63@gmail.com>
2023-02-06 20:29:44 +0900

    firebase

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvide } from 'firebase/auth';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "REACT_APP_FIREBASE_API_KEY",
-  authDomain: "REACT_APP_AUTH_DOMAIN",
-  databaseURL: "REACT_APP_DATABASE_URL",
-  projectId: "REACT_APP_PROJECT_ID",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
-const provider = new GoogleAuthProvide();
+const provider = new GoogleAuthProvider();
 
 export function login(){
   signInWithPopup(auth, provider)
@@ -19,7 +19,15 @@ export function login(){
     console.log(user);
   }).catch(console.error);
 }
+export function logout(){
+  signOut(auth).then(() => null);
+}
 
+export function onUserStateChange(callback){
+  onAuthStateChanged(auth, (user) => {
+    callback(user);
+  })
+}
 // Cart userId에 있는 cart 불어오기
 // export async function getCart(userId){
 //   return get(ref(database, `cart/${userId}`))
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { FiSmile } from 'react-icons/fi';
-import login from '../../firebase'
+import {login, logout, onUserStateChange} from '../api/firebase'
 
 function Navbar() {
+  const [user, setUser] = useState();
+
+  const handleLogin = () => {
+    login().then(setUser);
+  } 
+  const handleLogout = () => {
+    logout().then(setUser);
+  } 
+
+  useEffect(() => {
+    onUserStateChange((user) => {
+      console.log(user);
+      setUser(user);
+    })
+  }, [])
+
+
+
   return (
     <header class='font-serif flex justify-between border-b bg-white font-semibold p-2'>
         <Link  to='/' className='flex items-center gap-4 text-4xl'>
@@ -12,9 +30,12 @@ function Navbar() {
         </Link>
       <nav class='flex items-center gap-4 font-semibold text-2xl'>
         <Link to='/cart'>Cart</Link>
-        <Link  to='/product/new'>Login</Link>
+        <Link  to='/product/new'>New</Link>
         {/* <button >Login</button> */}
-        <button onClick={login}>Login</button>
+        { !user && <button onClick={handleLogin}>Login</button>}
+        { user && <button onClick={handleLogout}>Logout</button>}
+        
+
       </nav>
     </header>
   )
